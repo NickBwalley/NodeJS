06c9f479fa7c2fff9ac3d230db1750c527e22a79
NickBwalley <nickbwalley@gmail.com>
2021-11-01 08:51:48 +0300

    nodejs path module

--- node-module-system/app.js
+++ node-module-system/app.js
@@ -1,4 +1,7 @@
-const logger = require('./logger');
+const path = require('path');
+var pathObj = path.parse(__filename);
+console.log(pathObj);
 
+// const logger = require('./logger');
 // console.log(logger);
-logger('hello world');
+// logger('hello world');
