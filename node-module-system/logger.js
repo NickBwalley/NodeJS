adc8d1b7ca31dec25f9af6e1bc468eddb17db2d1
NickBwalley <nickbwalley@gmail.com>
2021-11-01 08:39:47 +0300

    module wrapper function

--- node-module-system/logger.js
+++ node-module-system/logger.js
@@ -1,8 +1,13 @@
 var url = 'http://mylogger.io/log';
-
+console.log(__filename);
+console.log(__dirname);
 function log(message) {
     // Send a HTTP request
     console.log(message);
 }
 
 module.exports = log;
+
+module.exports.log = log;
+// exports.log = log;
+// exports = log; // this is a bad approach.
