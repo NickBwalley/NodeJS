61141d03a18fd928cc55d1d98c8616c50ba367e7
NickBwalley <nickbwalley@gmail.com>
2021-11-01 09:10:03 +0300

    file system module

--- node-module-system/app.js
+++ node-module-system/app.js
@@ -1,11 +1,20 @@
+// FILE SYSTEM MODULE
+const fs = require('fs');
+const files = fs.readdirSync('./');
+fs.readdir('./', function (err, files) {
+    if (err) console.log('Error', err);
+    else console.log('Result', files);
+});
+console.log(files);
+
 // OS MODULE
-const os = require('os');
+/*const os = require('os');
 var totalMemory = os.totalmem();
 var freeMemory = os.freemem();
 // Template string.
 // ES6 / ES2015: ECMAScript 6
 console.log(`Total Memory:  ${totalMemory}`);
-console.log(`Free Memory:  ${freeMemory}`);
+console.log(`Free Memory:  ${freeMemory}`);*/
 
 // PATH MODULE
 /*const path = require('path');
