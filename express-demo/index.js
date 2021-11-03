431de34f39a5b9c8ae4696e87769f83c1c1f9c16
NickBwalley <nickbwalley@gmail.com>
2021-11-03 10:07:20 +0300

    handling http put requests

--- express-demo/index.js
+++ express-demo/index.js
@@ -17,12 +17,12 @@ app.get('/api/courses', (req, res) => {
   res.send(courses); // this is a route handler
 });
 
-// app.get('/api/courses/:id', (req, res) => {
-//   const course = courses.find((c) => c.id === parseInt(req.params.id));
-//   if (!course)
-//     res.status(404).send('The course with the given ID was not found!');
-//   res.send(course);
-// });
+app.get('/api/courses/:id', (req, res) => {
+  const course = courses.find((c) => c.id === parseInt(req.params.id));
+  if (!course)
+    res.status(404).send('The course with the given ID was not found!');
+  res.send(course);
+});
 
 app.post('/api/courses/', (req, res) => {
   const schema = {
@@ -34,14 +34,26 @@ app.post('/api/courses/', (req, res) => {
   if (result.error) {
     // 400 Bad Request!!
     res.status(400).send(result.error.details[0].message);
-    return;
   }
+  return;
   const course = {
     id: courses.length + 1,
     name: req.body.name,
   };
   courses.push(course);
   res.send(course);
+});
+
+app.put('/api/course/:id', (req, res) => {
+  const course = courses.find((c) => c.id === parseInt(req.params.id));
+  if (!course)
+    res.status(404).send('The course with the given ID was not found!');
+
+  //validate
+  // If invalide return 404 - Bad request
+
+  // update
+  // Return updated course
 });
 
 const port = process.env.PORT || 3000;
