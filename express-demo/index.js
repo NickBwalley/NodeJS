f45d12c4710cf81087bafc7b356979dc7face9c8
NickBwalley <nickbwalley@gmail.com>
2021-11-03 08:35:58 +0300

    setting up environment variables in expressjs

--- express-demo/index.js
+++ express-demo/index.js
@@ -2,9 +2,15 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello this is Nicholas Kibiy Bwalley!'); // this is a route handler
+  res.send('Hello World!!!'); // this is a route handler
 });
 app.get('/api/courses', (req, res) => {
   res.send([1, 2, 3]); // this is a route handler
 });
-app.listen(3000, () => console.log('Listening on port 3000...'));
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
+
+// Configuration in the terminal using the following commands
+// $ export PORT=5000 // this is for linux and mac users
+// $ set PORT=5000 // this is for windows users
