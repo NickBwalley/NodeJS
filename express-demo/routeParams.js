c7712aca1e7bd23597d86c045ecb591561fa3332
NickBwalley <nickbwalley@gmail.com>
2021-11-03 08:58:10 +0300

    handling HTTP GET requests

--- express-demo/routeParams.js
+++ express-demo/routeParams.js
@@ -1,24 +1,16 @@
 const express = require('express');
 const app = express();
 
-const courses = [
-  { id: 1, name: 'Java' },
-  { id: 2, name: 'JavaScript' },
-  { id: 3, name: 'Python' },
-];
 app.get('/', (req, res) => {
   res.send('Hello World!!!'); // this is a route handler
 });
 
 app.get('/api/courses', (req, res) => {
-  res.send(courses); // this is a route handler
+  res.send([1, 2, 3]); // this is a route handler
 });
 
-app.get('/api/courses/:id', (req, res) => {
-  const course = courses.find((c) => c.id === parseInt(req.params.id));
-  if (!course)
-    res.status(404).send('The course with the given ID was not found!');
-  res.send(course);
+app.get('/api/courses/:year/:month', (req, res) => {
+  res.send(req.params); // req.query // query string params to provide additional data to our backend services
 });
 
 const port = process.env.PORT || 3000;
