4028988d532f6df26bfadec262eb446a98b78480
NickBwalley <nickbwalley@gmail.com>
2021-11-04 12:16:25 +0300

    debugging

--- express-advanced-topics/index.js
+++ express-advanced-topics/index.js
@@ -1,3 +1,5 @@
+const startupDebugger = require('debug')('app:startup');
+const dbDebugger = require('debug')('app:db');
 const config = require('config');
 const morgan = require('morgan');
 const helmet = require('helmet');
@@ -16,14 +18,17 @@ app.use(express.static('public'));
 app.use(helmet());
 
 // Configuration
-console.log('Application Name: ' + config.get('name'));
-console.log('Mail Server: ' + config.get('mail.host'));
-console.log('Mail Password: ' + config.get('mail.password'));
+// console.log('Application Name: ' + config.get('name'));
+// console.log('Mail Server: ' + config.get('mail.host'));
+// console.log('Mail Password: ' + config.get('mail.password'));
 
 if (app.get('env') === 'development') {
   app.use(morgan('tiny'));
-  console.log('Morgan enabled...');
+  startupDebugger('Morgan enabled...');
 }
+
+//Db work
+dbDebugger('Connected to the database...');
 
 app.use(logger);
 app.use(authentication);
