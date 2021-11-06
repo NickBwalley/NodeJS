9046be14f299aab41730f9e1d646a0eaf65b5722
NickBwalley <nickbwalley@gmail.com>
2021-11-06 10:49:18 +0300

    saving a document in MongoDB

--- mongo-crud-app/index.js
+++ mongo-crud-app/index.js
@@ -1,22 +1,30 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-mongoose.connect('mongodb://localhost/playground')
-  .then(() => console.log('Connected to MongoDB...'))
-  .catch((err) => console.log('Could not connect to MongoDB...', err))
+mongoose
+  .connect("mongodb://localhost/playground")
+  .then(() => console.log("Connected to MongoDB..."))
+  .catch((err) => console.log("Could not connect to MongoDB...", err));
 
-const courseSchema =  new mongoose.Schema({
-  name: String, 
-  author: String, 
-  tags: [ String ],
-  date: { type: Date, default: Date.now},
-  isPublished: Boolean 
+const courseSchema = new mongoose.Schema({
+  name: String,
+  author: String,
+  tags: [String],
+  date: { type: Date, default: Date.now },
+  isPublished: Boolean,
 });
 
-// Classes and Objects
-const Course = mongoose.model('Course', courseSchema);
-const course = new Course({
-  name: 'Node.js course', 
-  author: 'Nick', 
-  tags: ['node', 'backend'],
-  isPublished: true
-});
+async function createCourse() {
+  // Classes and Objects
+  const Course = mongoose.model("Course", courseSchema);
+  const course = new Course({
+    name: "React.js course",
+    author: "Nick",
+    tags: ["react", "frontend"],
+    isPublished: true,
+  });
+
+  const result = await course.save();
+  console.log(result);
+}
+
+createCourse();
