575fcb79fef70a619a754f690836daee31f230f3
NickBwalley <nickbwalley@gmail.com>
2021-11-08 15:25:25 +0300

    mongo-exercise

---
+++ mongo-exercises/solution3.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/mongo-exercises')
+
+const coursesSchema = mongoose.Schema({
+  name: String, 
+  author: String, 
+  tags: [String],
+  date: Date, 
+  isPublished: Boolean, 
+  price: Number
+});
+
+const Course = mongoose.model('Course', coursesSchema);
+
+const getCourses = async () => {
+  return await Course
+    .find({isPublished: true})
+    .or([
+      {price: {$gte: 15}},
+      {name: /.*by.*/i}
+    ])
+    .sort('-price')
+    .select('name author price')
+}
+
+const run = async () => {
+  const courses = await getCourses();
+  console.log(courses);
+}
+run();
