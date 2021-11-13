de4c707dcfea8ac1d3bb978116daf58a5dc0f505
NickBwalley <nickbwalley@gmail.com>
2021-11-13 11:31:38 +0300

    authenticating users

--- authentication-and-authorization/vidly/models/user.js
+++ authentication-and-authorization/vidly/models/user.js
@@ -25,7 +25,7 @@ const User = mongoose.model('User', new mongoose.Schema({
 
 function validateUser(user) {
   const schema = {
-    user: Joi.string().min(5).max(50).required(),
+    name: Joi.string().min(5).max(50).required(),
     email: Joi.string().min(5).max(255).required().email(),
     password: Joi.string().min(5).max(255).required(),
   };
