58bc86ca533f35192ca6daeb81948b0c412c2552
NickBwalley <nickbwalley@gmail.com>
2021-11-17 10:12:44 +0300

    testing the input

--- TESTING/TDD/tests/integration/returns.test.js
+++ TESTING/TDD/tests/integration/returns.test.js
@@ -1,5 +1,6 @@
 const request = require('supertest');
 const {Rentals, Rental} = require('../../models/rental');
+const {User} = require('../../models/user');
 const mongoose = require('mongoose');
 
 describe('/api/returns', () => {
@@ -39,5 +40,27 @@ describe('/api/returns', () => {
       .send({customerId, movieId}); 
 
       expect(res.status).toBe(401);
+  });
+
+  it('should return 400 if customerId is not provided', async () => {
+    const token = new User().generateAuthToken();
+    
+    const rest = await request(server)
+      .post('/api/returns')
+      .set('x-auth-token', token)
+      .send({movieId}); 
+
+      expect(res.status).toBe(400);
+  });
+
+  it('should return 400 if movieId is not provided', async () => {
+    const token = new User().generateAuthToken();
+    
+    const rest = await request(server)
+      .post('/api/returns')
+      .set('x-auth-token', token)
+      .send({ customerId }); 
+
+      expect(res.status).toBe(400);
   });
 });
